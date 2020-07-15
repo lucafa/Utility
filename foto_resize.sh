512da3506e2b9337d23813d9c58698ad941caeb1
Luca Facchin <32875729+lucafa@users.noreply.github.com>
2020-07-15 15:52:02 +0200

    Create foto_resize.sh

---
+++ foto_resize.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for i in `find . -iname '*.jpg' -type f`
+do
+	
+	if [[ -f $i ]]; then
+		SIZE=`stat --printf="%s" $i`
+		ABS_PATH=${i#?};
+		DEST_PATH="/home/luca/foto_resize"$ABS_PATH
+		FOLDER=`dirname $DEST_PATH`
+		if [[ ! -d $FOLDER ]];then
+			mkdir  -p $FOLDER
+		fi
+		if [[  $SIZE -gt 3000000 ]];then
+			convert -resize 25% $i $DEST_PATH
+		elif [[ $SIZE -le 1000000 ]]; then
+			cp $i $DEST_PATH
+		else 
+			convert -resize 50% $i $DEST_PATH
+		fi
+	fi	
+	
+done
